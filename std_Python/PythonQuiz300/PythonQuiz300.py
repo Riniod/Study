efdb992010f3662d49c419582c47482595b9de70
th <kth3969@naver.com>
2022-02-10 20:02:01 +0900

    22.2.10 ,파이썬퀴즈300 풀이 ( ~ 006번 )

---
+++ std_Python/PythonQuiz300/PythonQuiz300.py
@@ -0,0 +1,59 @@
+
+def Quiz001():
+    print("001 print 기초")
+    print("화면에 Hello World 문자열을 출력하세요\n")
+    print("정답 :\n","Hello World")
+    print("\n")    
+
+def Quiz002():
+    print("002 print 기초")
+    print("화면에 Mary's cosmetics을 출력하세요. (중간에 '가 있음에 주의하세요)")
+
+    print("\n정답 : ")
+    print("Mary's cosmetics")
+    print("\n")    
+
+def Quiz003():
+    print("003 print 기초")
+    print('화면에 아래 문장을 출력하세요. (중간에 "가 있음에 주의하세요.)')
+    print('신씨가 소리질렀다. "도둑이야".') 
+
+    print("\n정답 : ")
+    print('신씨가 소리질렀다. "도둑이야".')
+    print("\n")    
+
+
+def Quiz004():
+    print("004 print 기초") 
+    print('화면에 "C:\Windows"를 출력하세요.') 
+
+    print("\n정답 : ")
+    print('"c:\Windows"')
+    print("\n")    
+
+
+    
+def Quiz005():
+    print("005 print 탭과 줄바꿈")
+    print("다음 코드를 실행해보고 \\t와 \\n의 역할을 설명해보세요.")
+    print("안녕하세요.\\n만나서\\t\\t반갑습니다.")
+
+    print("\n정답 :")
+
+    print("\\n : 한줄 띄우기")
+    print("\\t : 탭 간격 띄우기 ( 4칸 )")
+    print("\n")    
+
+
+
+def Quiz006():
+    print("# 006 print 여러 데이터 출력")
+    print("print 함수에 두 개의 단어를 입력한 예제입니다. 아래 코드의 출력 결과를 예상해봅시다.")
+    print('print ("오늘은", "일요일")')
+    
+    print("\n정답 : ")
+    print("print 문 내에서 , 는 한칸 띄워주는 역할을 하므로 '오늘은' 다음에 한칸 띄운 후 '일요일'이 출력 된다")
+    print("오늘은", "일요일")
+    print("\n")    
+
+
